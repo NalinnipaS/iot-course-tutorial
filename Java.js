b8f542e6e8b1474f381855006c72c247fdd56e2d
Nalinnipa Sutthi <s6401072510168@email.kmutnb.ac.th>
2023-10-21 19:56:01 +0700

    create CSS.css

---
+++ Java.js
@@ -0,0 +1,25 @@
+var faceft = document.getElementById("facebook");
+faceft.addEventListener("click",function(){
+    window.open("https://web.facebook.com/profile.php?id=100004626312913","_blank");
+});
+
+var lineft = document.getElementById("line");
+lineft.addEventListener("click",function(){
+    window.open("https://line.me/ti/p/xWjvD-QWqf","_blank");
+});
+
+var instagramft = document.getElementById("instagram");
+instagramft.addEventListener("click",function(){
+    window.open("https://instagram.com/tle_nalin_?igshid=MzMyNGUyNmU2YQ==","_blank");
+});
+
+var githubft = document.getElementById("github");
+githubft.addEventListener("click",function(){
+    window.open("https://github.com/NalinnipaS","_blank");
+});
+
+var emailft = document.getElementById("email");
+emailft.addEventListener("click",function(){
+    window.open("mailto:s6401072510168@email.kmutnb.ac.th","_blank");
+});
+
